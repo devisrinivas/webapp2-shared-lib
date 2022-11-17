6a962e78af87fcc141e5dbc11fc2f7db44f8d005
devisrinivas <114465949+devisrinivas@users.noreply.github.com>
2022-11-17 15:26:15 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,8 +1,8 @@
 def call(creds,tomcatIp,userName,warName) {
   sshagent([creds]) {
     // copy war file to tomcat webapps
-    sh "scp -o StrictHostkeyChecking=no target/${warName}.war ${userName}@${tomcatIp}:/opt/tomcat9/webapps/"
-    sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/shutdown.sh"
-    sh "ssh ${userName}@${tomcatIp}/opt/tomcat9/startup.sh"
+    sh "scp target/${warName}.war ${userName}@${tomcatIp}:/opt/tomcat9/webapps/"
+    sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/shutdown.sh"
+    sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/startup.sh"
     }
 }
